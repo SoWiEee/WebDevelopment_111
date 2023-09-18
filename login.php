b5e6e87d4f2f589939d1b63df52b5d14707538eb
SoWiEee <99129463+SoWiEee@users.noreply.github.com>
2023-09-18 11:28:30 +0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<?php
+  printf("你輸入的帳號是：%s，密碼是：%s", $_POST["userId"], $_POST["userPw"]);
+  printf("你的性別是：%s，年齡是：%s", $_POST["userId"], $_POST["userPw"]);
+  printf("幸運數字是 %s <br>", join($_POST["lucky"],","));
+
+json ["1":"1","name":"2",3:"3"]
+?> 
+
+</body>
+</html>
